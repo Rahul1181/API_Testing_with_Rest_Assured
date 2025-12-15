dc871f690eeba34292f3210d37ed7b4a4d66e848
Rahul Mandal <rahulmandal99555@gmail.com>
2025-12-16 03:59:28 +0530

    file: New PostDataTest.java file is created and added the dependencies in readme.md file and added the getListResoureces method

--- api-automation/src/test/java/api_testing/Reqres/GetDataTest.java
+++ api-automation/src/test/java/api_testing/Reqres/GetDataTest.java
@@ -1,15 +1,16 @@
 package api_testing.Reqres;
 
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItemInArray;
+import static org.hamcrest.Matchers.hasItems;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import api_testing.config.EnvironmentConfig;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
-
-import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
 
 public class GetDataTest {
     @Test
@@ -28,6 +29,12 @@ public class GetDataTest {
         System.out.println("Response body is :" + usersList.getBody().asString());
         System.out.println("Header Details : " + usersList.getHeader("content-type"));
 
+        given()
+            .header("x-api-key", EnvironmentConfig.getReqresApiKey())
+            .get("https://reqres.in/api/users?page=2")
+        .then()
+            .body("data.last_name", hasItems("Ferguson", "Edwards"));
+
         int statusCode = usersList.getStatusCode();
         Assert.assertEquals(statusCode, 200);
 
@@ -45,8 +52,15 @@ public class GetDataTest {
         given().header("x-api-key", EnvironmentConfig.getReqresApiKey()).get("users/2")
         .then().body("data.first_name", equalTo("Janet")).log().all();
 
-
-
+    }
+    @Test
+    public void getListResources(){
+        baseURI = "https://reqres.in/api/";
+        given()
+            .header("x-api-key", EnvironmentConfig.getReqresApiKey()).get("unknown")
+        .then()
+            .body("data[0].name", equalTo("cerulean"));
+            
     }
 
 }
---
+++ api-automation/src/test/java/api_testing/Reqres/PostDataTest.java
@@ -0,0 +1,48 @@
+package api_testing.Reqres;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.text.AbstractDocument.Content;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import api_testing.config.EnvironmentConfig;
+import io.restassured.http.ContentType;
+
+public class PostDataTest {
+    @Test
+    public void postCreate(){
+        //Map<String, Object> newUserData = new HashMap<String, Object>();
+        // newUserData.put("name", "Rahul");
+        // newUserData.put("job", "private employee");
+
+        baseURI ="https://reqres.in/api/";
+
+        //JSON format
+        JSONObject jsonFormat = new JSONObject();
+        jsonFormat.put("name", "Rahul");
+        jsonFormat.put("job", "private employee");
+        System.out.println(jsonFormat);
+
+        given()
+            .header("x-api-key", EnvironmentConfig.getReqresApiKey())
+            .header("Content-Type", "application/json")
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .body(jsonFormat.toJSONString())
+        .when()
+            .post("users")
+        .then()
+            .statusCode(201).log().all();
+
+
+
+
+    }
+
+}
