b6e1513cfcdde12fe6741d0d8223ee62030e951d
Rahul Mandal <rahulmandal99555@gmail.com>
2025-12-15 22:51:45 +0530

    Fix: setup of API key and response

--- api-automation/src/test/java/api_testing/Reqres/GetDataTest.java
+++ api-automation/src/test/java/api_testing/Reqres/GetDataTest.java
@@ -3,25 +3,29 @@ package api_testing.Reqres;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import api_testing.config.EnvironmentConfig;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
 
 public class GetDataTest {
     @Test
     public void userData() {
-        // Stores the response
-        // Response bookingID = RestAssured.get("https://restful-booker.herokuapp.com/booking?firstname=Mary&lastname=Jackson");
-        
-        // System.out.println("Status code is :" + bookingID.getStatusCode());
-        
-        // System.out.println("Time taken to find the data: " + bookingID.getTime());
-        
-        // System.out.println("Response body is :" + bookingID.getBody().asString());
-    
-        // System.out.println("Header Details : " + bookingID.getHeader("content-type"));
+        // 2 ways to Stores the response
+        // Response userInfo = RestAssured.get("https://reqres.in/api/users?page=2");
 
-        // int statusCode = bookingID.getStatusCode();
-        // Assert.assertEquals(statusCode, 200);
+        // Fluent way to Stores the response
+        Response userInfo = (Response) given()
+                .header("x-api-key", EnvironmentConfig.getReqresApiKey())
+                .when().get("https://reqres.in/api/users?page=2");
+
+        System.out.println("Status code is :" + userInfo.getStatusCode());
+        System.out.println("Time taken to find the data: " + userInfo.getTime());
+        System.out.println("Response body is :" + userInfo.getBody().asString());
+        System.out.println("Header Details : " + userInfo.getHeader("content-type"));
+
+        int statusCode = userInfo.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
 
     }
 
