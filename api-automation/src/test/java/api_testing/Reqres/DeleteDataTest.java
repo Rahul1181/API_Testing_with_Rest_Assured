629eead4e5b5a9aec9c50b1f5bf708ff4ff6fae8
Rahul Mandal <rahulmandal99555@gmail.com>
2025-12-16 17:14:29 +0530

    new: Setup the Delete API request

---
+++ api-automation/src/test/java/api_testing/Reqres/DeleteDataTest.java
@@ -0,0 +1,24 @@
+package api_testing.Reqres;
+
+import org.testng.annotations.Test;
+
+import api_testing.config.EnvironmentConfig;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;;
+
+public class DeleteDataTest {
+    @Test
+    public void deleteUserData(){
+        baseURI ="https://reqres.in/api/";
+
+        given()
+            .header("x-api-key", EnvironmentConfig.getReqresApiKey())
+        .when()
+            .delete("users/2")
+        .then()
+            .statusCode(204).log().all();
+
+    }
+    
+}
